f8cb8cdb4ad788986a3ea8aecb5af98df8edf124
hadiudoit11 <85430282+hadiudoit11@users.noreply.github.com>
2022-08-07 02:35:54 -0400

    great balls of fire

--- app.py
+++ app.py
@@ -2,34 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '5b3cd5b80eb8b217c20fb37074ff4a33'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///default.db'
-# db = SQLAlchemy(app)
-#
-#
-# class Movie(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(128), unique=True, nullable=False)
-#     release_date = db.Column(db.Date(), nullable=False)
-#
-#     def as_dict(self):
-#         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-#
-#     def __repr__(self):
-#         return '<Movie %r>' % self.title
 
-
-@app.route('/')
-def index():
-    return 'flask-movies'
-
-
-@app.route('/api/movies')
-def api_movies():
-    json = [movie.as_dict() for movie in Movie.query.all()]
-    return jsonify(json)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
