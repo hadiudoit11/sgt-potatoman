0161376e911bc82ce1d1783b1b17d1baefd74c8b
hadiudoit11 <85430282+hadiudoit11@users.noreply.github.com>
2022-08-11 23:02:00 -0400

    database update

--- application.py
+++ application.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, request
+from get_Database import runGetDatabase as runGetDatabase
+import psycopg2
+
 
 application = Flask(__name__)
 
-@application.route("/healthcheck")
-def healthcheck():
-    status_code = flask.Response(status=200)
-    return status_code
+DB_NAME = "jwjuiwdg"
+DB_USER = "jwjuiwdg"
+DB_PASS = "o8dUXLTQev50L-yuZ8F8mRllAvFCoNke"
+DB_HOST = "rajje.db.elephantsql.com"
+DB_PORT = "5432"
 
-@application.route("/")
-def hello_world():
+conn = psycopg2.connect(database = DB_NAME, user = DB_USER,
+                            password = DB_PASS, host = DB_HOST, port = DB_PORT)
 
-    return 'poo poo pee pee'
+cur = conn.cursor()
+cur.execute("""SELECT body_Review FROM review_data;""")
+database_Response = cur.fetchall()
 
+body_Response = {
+    'body_Review': []
+}
+body_Response['body_Review'].append('{}'.format(database_Response))
 
-if __name__ == '__main__':
+@application.route("/", methods=['GET','POST'])
+def home():
+    return body_Response
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
